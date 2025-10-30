9e316e85b3005f9546c0447103a4fec17d6233b9
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-10-30 14:58:00 +0100

    *

--- app/page.jsx
+++ app/page.jsx
@@ -17,7 +17,7 @@ export default async function Home() {
 		const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}api/games/game-get-all`, {
 			next: { tags: ['games-list'] },
 		})
-		const { success, data } = await res.json()
+		const { success, payload: data } = await res.json()
 		payload = data
 		ok = success
 	}
